c5de52f4c65aa82767e04cea6515f7936bec1daf
Davran5 <devron.ra@gmail.com>
2026-04-24 20:08:26 +0500

    chore: ignore build artifacts and internal files

--- .gemini/antigravity/brain/5e6c45a1-6b2c-4b5b-8fb9-5b5a154329f9/scratch/test_api_page.js
+++
@@ -1,6 +0,0 @@
-async function test() {
-    const res = await fetch('http://localhost:10000/api/pages/home?lang=ru');
-    const data = await res.json();
-    console.log(JSON.stringify(data, null, 2));
-}
-test();
